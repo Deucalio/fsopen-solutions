0edd173ca8a3fc519daa381013cce77831cfd33f
Deucalio <hjamad89@gmail.com>
2023-02-23 00:21:43 +0500

    update readme and refactor app

--- part1/README.md
+++ part1/README.md
@@ -2,6 +2,8 @@
 
 In this part, we will familiarize ourselves with the React-library while also looking at some features of JavaScript that are cruical to understanding React.
 
-#### [Unicafe](./unicafe)
-#### [Course Information](./courseinfo)
-#### [Anecdotes](./anecdotes)
+## Exercises
+
+- [Unicafe](./unicafe) - WIP
+- [Course Information](./courseinfo) - A very basic React App which displays course information
+- [Anecdotes](./anecdotes) - WIP
--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,7 +1,7 @@
-const Part = ({part, exercises}) => {
+const Part = ({part}) => {
   return (
     <p>
-      {part} {exercises}
+      {part.name} {part.exercises}
     </p>
   )
 }
@@ -10,15 +10,12 @@ const Content = ({
   part1,
   part2,
   part3,
-  exercises1,
-  exercises2,
-  exercises3,
 }) => {
   return (
     <>
-    <Part part={part1} exercises={exercises1}/>
-    <Part part={part2} exercises={exercises2}/>
-    <Part part={part3} exercises={exercises3}/>
+    <Part part={part1}/>
+    <Part part={part2}/>
+    <Part part={part3}/>
     </>
   );
 };
