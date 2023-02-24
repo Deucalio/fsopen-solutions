36572e12b0f2dd2608a19d9d6da8e6f81a13a091
Deucalio <hjamad89@gmail.com>
2023-02-24 15:40:01 +0500

    submit ex 2.3

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -8,8 +8,20 @@ const Part = ({ part: { name, exercises } }) => {
   );
 };
 
+const Sum = ({ parts }) => {
+  const sum = parts.reduce((acc, currentVal) => acc + currentVal.exercises, 0);
+  return <h3>Total of {sum} exercises</h3>;
+};
+
 const Content = ({ content: { parts } }) => {
-  return parts.map((p) => <Part key={p.id} part={p} />);
+  return (
+    <>
+      {parts.map((p) => (
+        <Part key={p.id} part={p} />
+      ))}
+      <Sum parts={parts} />
+    </>
+  );
 };
 
 export default Content;
