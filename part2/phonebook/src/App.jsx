5cda23fc8ba7158af5ecfe05abbf1e594473640c
Deucalio <hjamad89@gmail.com>
2023-02-24 22:41:26 +0500

    submit ex 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,42 @@
 import { useState } from "react";
 
+const Person = ({ persons, filterText }) => {
+  const filteredPeople = persons.map((person, i) => {
+    // if filterText is not person's name
+    if (person.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+      return;
+    }
+    return (
+      <p key={i}>
+        {person.name} {person.number}
+      </p>
+    );
+  });
+
+  console.log("filteredPeople", filteredPeople);
+
+  return <>{filteredPeople}</>;
+};
+
+const SearchBar = ({ filterText, setFilterText }) => {
+  return (
+    <form>
+      <div>
+        filter shown with{" "}
+        <input
+          type="text"
+          value={filterText}
+          onChange={(e) => setFilterText(e.target.value)}
+        />
+      </div>
+    </form>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newPerson, setNewPerson] = useState({ name: "", number: "" });
+  const [filterText, setFilterText] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -29,40 +63,41 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h2>Phonebook</h2>
+    <>
+      <div>
+        <h2>Phonebook</h2>
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          name:{" "}
-          <input
-            name="name"
-            value={newPerson.name}
-            type="text"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          number:{" "}
-          <input
-            name="number"
-            value={newPerson.number}
-            type="text"
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">add</button>
-      </form>
+        <SearchBar filterText={filterText} setFilterText={setFilterText} />
 
-      <form onSubmit={handleSubmit}></form>
+        <legend>
+          <h2>Add a new</h2>
+        </legend>
+        <form onSubmit={handleSubmit}>
+          <div>
+            name:{" "}
+            <input
+              name="name"
+              value={newPerson.name}
+              type="text"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            number:{" "}
+            <input
+              name="number"
+              value={newPerson.number}
+              type="text"
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">add</button>
+        </form>
 
-      <h2>Numbers</h2>
-      {persons.map((person, i) => (
-        <p key={i}>
-          {person.name} {person.number}
-        </p>
-      ))}
-    </div>
+        <h2>Numbers</h2>
+        <Person persons={persons} filterText={filterText} />
+      </div>
+    </>
   );
 };
 
