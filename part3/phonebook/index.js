4e8d6eea958d062df5aa05235fa60bfc518111ad
Deucalio <hjamad89@gmail.com>
2023-02-27 17:47:11 +0500

    done ex 3.13

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,40 +2,54 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const cors = require("cors");
+require("dotenv").config();
 app.use(cors());
+const People = require("./models/people");
+const mongoose = require("mongoose");
 
 app.use(express.json()); //useful if you're working with req.body obj
 
-let data = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// let data = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 // fetches every person
-app.get("/api/persons", (req, res) => {
-  res.status(200).json(data);
+app.get("/api/persons", async (req, res) => {
+  const persons = await People.find({});
+  res.status(200).json(persons);
 });
 
 //  add a new person
