a9b9c4267b85bea9ede82ecdf07aea4f26401a78
Deucalio <hjamad89@gmail.com>
2023-02-28 22:59:00 +0500

    exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -5,7 +5,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const favoriteBlog  = (blogs) => {
+  // returns the blog with the most likes
+
+  // sort the blog in descending order
+  blogs.sort(function (a, b) {
+    if (a['likes'] < b['likes']) return 1
+    if (a['likes'] > b['likes']) return -1
+    return 0
+  })
+  return blogs[0]
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog 
 };
