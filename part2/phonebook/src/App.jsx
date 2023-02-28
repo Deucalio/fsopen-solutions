5a51b3a431d86842f79007a61d1c458b48a2e534
Deucalio <hjamad89@gmail.com>
2023-02-28 15:29:22 +0500

    add validation for phone number

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -70,6 +70,8 @@ const App = () => {
     setNewPerson({ ...newPerson, [name]: value });
   };
 
+  const isValidNumber = numberStr => /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g.test(numberStr)
+
   const updatePerson = async (id) => {
     const res = await axios.put(`${baseUrl}/api/${id}`, newPerson);
     const updatedPerson = res.data;
@@ -102,6 +104,18 @@ const App = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (newPerson.name === "" || newPerson.number === "") {
+      return alert("Invalid input");
+    }
+    if (newPerson.name.trim().length < 3) {
+      return alert(
+        "validation failed! name is shorter than minimun allowed length (3)"
+      );
+    }
+    if (newPerson.number.length < 8 || !(isValidNumber(newPerson.number)) ){
+      return alert("incorrect Number format")
+    }
 
     // check if the name already exists
     const userExists =
