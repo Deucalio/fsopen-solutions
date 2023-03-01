605451653dc9ef8040c9903aaa042370af08e31c
Deucalio <hjamad89@gmail.com>
2023-03-01 23:00:11 +0500

    4.12

--- part4/bloglist/controllers/blogsController.js
+++ part4/bloglist/controllers/blogsController.js
@@ -6,6 +6,10 @@ exports.getAllBlogs = async (req, res) => {
 };
 
 exports.postABlog = async (req, res) => {
+  const { title, url } = req.body;
+  if (!title || !url) {
+    return res.status(400);
+  }
   const blog = await Blog.create({ ...req.body, likes: req.body.likes || 0 });
   const newBlog = await blog.save();
   res.status(201).json(newBlog);
