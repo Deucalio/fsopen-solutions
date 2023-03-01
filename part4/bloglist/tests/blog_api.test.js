edd558a36079ad30fedf6a6526006aa6164e2b69
Deucalio <hjamad89@gmail.com>
2023-03-01 21:14:35 +0500

    ex 4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,14 +1,29 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const Blog = require("../models/Blog");
 
 const api = supertest(app);
 
-test("notes are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+const getBlogs = async () => {
+  const blogs = await Blog.find({});
+  return blogs.map((b) => b.toJSON());
+  //   return blogs.toJSON();
+};
+
+// test("notes are returned as json", async () => {
+//   await api
+//     .get("/api/blogs")
+//     .expect(200)
+//     .expect("Content-Type", /application\/json/);
+// });
+
+test("test if a blog has id or _id", async () => {
+  const blogs = await getBlogs();
+  console.log("blogs", blogs);
+  //   await expect( getBlogs()).resolves.toBe(!undefined)
+  expect(blogs[0]._id).toBe(undefined);
+  expect(blogs[0].id).toBeDefined();
 });
 
 afterAll(async () => {
