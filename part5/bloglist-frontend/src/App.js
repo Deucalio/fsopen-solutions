d33d0694cf03c3132b486cbce4e652fc8fae7779
Deucalio <hjamad89@gmail.com>
2023-03-04 13:51:47 +0500

    ex 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -6,6 +6,7 @@ import axios from "axios";
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const [name, setName] = useState("");
   const [loginFormData, setLoginFormData] = useState({
     username: "",
     password: "",
@@ -14,9 +15,22 @@ const App = () => {
 
   const getBlogs = async (username) => {
     const users = await blogService.getAll();
-    const blogs = users.find((u) => u.username === username).blogs;
-    setBlogs(blogs);
+    const blogs = users.find((u) => u.username === username);
+    setName(blogs.name);
+    setBlogs(blogs.blogs);
   };
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    const username = JSON.parse(localStorage.getItem("username"));
+    if (token) {
+      getBlogs(username);
+      setUser(true);
+      console.log("exists");
+    } else {
+      console.log("doesn't exists");
+    }
+  }, [user]);
 
   const handleLoginForm = async (e) => {
     e.preventDefault();
@@ -29,10 +43,18 @@ const App = () => {
     );
     const { token, username } = req.data;
     if (token) {
+      localStorage.setItem("token", JSON.stringify(token));
+      localStorage.setItem("username", JSON.stringify(username));
       getBlogs(username);
       setUser(true);
     }
   };
+
+  const logOut = () => {
+    localStorage.clear()
+    setUser(null)
+  }
+
   if (user === null) {
     return (
       <Login
@@ -45,6 +67,9 @@ const App = () => {
 
   return (
     <div>
+      <h2>blogs</h2>
+      <h3>{name} logged in</h3>
+      <button onClick={logOut}>logout</button>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
