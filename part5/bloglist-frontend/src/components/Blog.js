db60351c1a47dd6507a8d47e4678d90b91068adb
Deucalio <hjamad89@gmail.com>
2023-03-04 15:24:10 +0500

    ex 5.8

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,21 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React from "react";
+import { useState } from "react";
+const Blog = ({ blog }) => {
+  const [displayDetails, setDisplayDetails] = useState(false);
+  return (
+    <div style={{ padding: "0.2rem", fontSize: "1.2rem" }}>
+      {blog.title}{" "}
+      <button onClick={() => setDisplayDetails(!displayDetails)}>
+        {displayDetails ? "hide" : "view"}
+      </button>
+      {displayDetails && (
+        <>
+          <p>{blog.url}</p>
+          <p>likes: {blog.likes}</p>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
