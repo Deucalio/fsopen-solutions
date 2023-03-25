2af65d9c37bd903c5b8cd932a32db1db4cecf448
Deucalio <hjamad89@gmail.com>
2023-03-25 19:28:29 +0500

    complete part6c

--- part6/anecdote-reactquery/src/App.jsx
+++ part6/anecdote-reactquery/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const anecdotesQuery = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: () => {
+      return axios
+        .get("http://localhost:3001/anecdotes")
+        .then((res) => res.data);
+    },
+  });
+
+  if (anecdotesQuery.status === "loading") return <h1>Loading</h1>;
+  if (anecdotesQuery.status === "loading")
+    return <h1>{JSON.stringify(anecdotesQuery.error)}</h1>;
 
   return (
     <div className="App">
+      <h1>Anecdotes app</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {anecdotesQuery.data.map((a) => {
+          return (
+            <>
+              <p>
+                {a.content}
+                <br />
+                <span>has {a.votes}</span>
+                <button>vote</button>
+              </p>
+            </>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
