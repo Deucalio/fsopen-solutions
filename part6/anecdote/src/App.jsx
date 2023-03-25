e92bc3eb187b1567e5c600453c80b227884c49f8
Deucalio <hjamad89@gmail.com>
2023-03-25 19:28:52 +0500

    complete part6c

--- part6/anecdote/src/App.jsx
+++ part6/anecdote/src/App.jsx
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import AddAnecdote from "../features/anecdotes/AddAnecdote";
 import AnecdoteList from "../features/anecdotes/AnecdoteList";
+import anecdoteService from "../features/anecdotes/anecdoteService";
+import { setAnecdotes } from "../features/anecdotes/anecdotesSlice";
 
 function App() {
   const [filter, setFilter] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getAnecdotes = async () => {
+      const res = await anecdoteService.getAll();
+      dispatch(setAnecdotes(res));
+    };
+    getAnecdotes();
+  }, []);
+
+  
+
   return (
     <div className="App">
       <h2>Anecdotes</h2>
